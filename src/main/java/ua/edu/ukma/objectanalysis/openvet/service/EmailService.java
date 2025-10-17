d79040fd61ca35efefc5fc4fa9788d069a2de8a7
Vladyslav Hibskyi <vgibskiy@gmail.com>
2025-10-18 00:13:59 +0300

    add email server

---
+++ src/main/java/ua/edu/ukma/objectanalysis/openvet/service/EmailService.java
@@ -0,0 +1,26 @@
+package ua.edu.ukma.objectanalysis.openvet.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    @Value("${spring.mail.username}")
+    private String sender;
+
+    private final JavaMailSender mailSender;
+
+    public void sendEmail(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        message.setFrom(sender);
+        mailSender.send(message);
+    }
+}
