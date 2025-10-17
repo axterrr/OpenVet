b10ff925896ce390af29ea7b6fc9c8b045679d72
Vladyslav Hibskyi <vgibskiy@gmail.com>
2025-10-18 01:06:10 +0300

    enable scheduling

--- src/main/java/ua/edu/ukma/objectanalysis/openvet/OpenVetApplication.java
+++ src/main/java/ua/edu/ukma/objectanalysis/openvet/OpenVetApplication.java
@@ -3,9 +3,11 @@ package ua.edu.ukma.objectanalysis.openvet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import ua.edu.ukma.objectanalysis.openvet.security.JwtProperties;
 
 @SpringBootApplication
+@EnableScheduling
 @EnableConfigurationProperties(JwtProperties.class)
 public class OpenVetApplication {
 
